aaa4710cc5ac4c6e00ee464144d2337cb634d9da
ms-tristan <tristan.masse@epitech.eu>
2024-06-20 13:45:59 +0200

    [ADD] vector_t class

---
+++ includes/snake.h
@@ -0,0 +1,5 @@
+typedef struct vector_s {
+    int x;
+    int y;
+} vector_t;
+
--- sources/classes/snake.cpp
+++ sources/classes/snake.cpp
@@ -1,2 +1,22 @@
 #include "../../includes/snake.h"
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+class Snake {
+private:
+    vector<vector_t> body;
+    vector_t direction;
+
+
+public:
+
+    void move() {
+        vector_t head = body[0];
+        vector_t new_head = {head.x + direction.x, head.y + direction.y};
+        body.insert(body.begin(), new_head);
+        body.pop_back();
+    }
+
+};
